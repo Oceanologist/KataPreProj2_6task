c63f3452448e79680189e4c3423d7fd07aa8fe11
Dudko Daniil <email.Dudko@gmail.com>
2025-04-09 15:58:47 +0300

    добавил калькулятор по алишеву

--- src/main/java/dao/CarDaoImpl.java
+++ src/main/java/dao/CarDaoImpl.java
@@ -1,5 +1,13 @@
 package dao;
 
+import model.Car;
+
+import java.util.List;
+
 public class CarDaoImpl implements CarDao{
 
+    @Override
+    public List<Car> listUsers() {
+        return null;
+    }
 }
